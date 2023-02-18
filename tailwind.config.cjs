32bcc79188b19a14646c204428aa726f270dfdc2
zfl4wless <noah.bauditz@live.de>
2023-02-18 23:24:30 +0100

    fix: colors in tailwind config not working

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
         extend: {
             colors: {
-                'blue-marguerite': {
+                primary: {
                     50: '#F0F2FD',
                     100: '#E4E7FB',
                     200: '#CFD4F6',
@@ -16,7 +18,7 @@ module.exports = {
                     800: '#4D4192',
                     900: '#413A75',
                 },
-                shark: {
+                neutral: {
                     50: '#F6F6F7',
                     100: '#E1E4E6',
                     200: '#C2C8CD',
@@ -28,8 +30,6 @@ module.exports = {
                     800: '#33373C',
                     900: '#202225',
                 },
-                primary: 'colors.blue-marguerite',
-                neutral: 'colors.shark',
             },
             fontFamily: {
                 'open-sans': ['Open Sans', 'sans-serif'],
