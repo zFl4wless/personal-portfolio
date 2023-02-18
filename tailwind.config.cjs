36f5283d628d9306f8cd9b75a47a68c3e8e05a6f
zfl4wless <noah.bauditz@live.de>
2023-02-18 23:14:57 +0100

    feat: adds color pallets to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,34 @@ module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
         extend: {
+            colors: {
+                'blue-marguerite': {
+                    50: '#F0F2FD',
+                    100: '#E4E7FB',
+                    200: '#CFD4F6',
+                    300: '#B2B7EF',
+                    400: '#9293E7',
+                    500: '#7E78DC',
+                    600: '#6A5ACD',
+                    700: '#5E4DB5',
+                    800: '#4D4192',
+                    900: '#413A75',
+                },
+                shark: {
+                    50: '#F6F6F7',
+                    100: '#E1E4E6',
+                    200: '#C2C8CD',
+                    300: '#9CA3AC',
+                    400: '#777F8A',
+                    500: '#5C6570',
+                    600: '#485059',
+                    700: '#3C4149',
+                    800: '#33373C',
+                    900: '#202225',
+                },
+                primary: 'colors.blue-marguerite',
+                neutral: 'colors.shark',
+            },
             fontFamily: {
                 'open-sans': ['Open Sans', 'sans-serif'],
             },
