778e88175d15392da0b880e634a76c6676a6dc9c
zfl4wless <noah.bauditz@live.de>
2023-02-19 22:14:09 +0100

    feat: adds header

---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,11 @@
+<header class="flex justify-between items-center px-6 py-3">
+    <img src="img/Logo.png" alt="Logo" width={110} />
+    <div class="flex items-center gap-4">
+        <a href="https://github.com/zFl4wless">
+            <img src="icons/github.svg" alt="GitHub Logo" height={32} width={32} />
+        </a>
+        <a href="https://www.instagram.com/noah.bdtz/">
+            <img src="icons/instagram.svg" alt="Instagram Logo" height={32} width={32} />
+        </a>
+    </div>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script>
+    import Header from '$lib/components/layout/Header.svelte';
     import '../app.css';
 </script>
 
-<h1>This page will transform into a beautiful portfolio soon!</h1>
+<main>
+    <!-- Hero Section -->
+    <section class="hero-section min-h-screen">
+        <Header />
+    </section>
+</main>
+
+<style>
+    .hero-section {
+        background-image: radial-gradient(ellipse at center, #1b2735 0%, #202225 60%);
+    }
+</style>
