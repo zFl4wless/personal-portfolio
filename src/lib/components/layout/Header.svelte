cb53f9107f1a441629d24d4f2ded7351343300d0
zfl4wless <noah.bauditz@live.de>
2023-02-20 16:30:01 +0100

    feat: adds buymeacoffe to social icons

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,11 +1,14 @@
 <header class="flex justify-between items-center py-3">
     <img src="img/Logo.png" alt="Logo" width={110} />
-    <div class="flex items-center gap-5">
+    <div class="flex items-center gap-6">
         <a class="transition ease-in-out hover:opacity-75 cursor-pointer" href="https://github.com/zFl4wless">
             <img src="icons/github.svg" alt="GitHub Logo" height={32} width={32} />
         </a>
         <a class="transition ease-in-out hover:opacity-75 cursor-pointer" href="https://www.instagram.com/noah.bdtz/">
             <img src="icons/instagram.svg" alt="Instagram Logo" height={32} width={32} />
+        </a>
+        <a class="transition ease-in-out hover:opacity-75 cursor-pointer" href="https://www.buymeacoffee.com/noahbdtz">
+            <img src="icons/buymeacoffee.svg" alt="Buymeacoffee Logo" height={32} width={32} />
         </a>
     </div>
 </header>
