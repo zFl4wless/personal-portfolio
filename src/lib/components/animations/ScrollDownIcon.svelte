a81646a7179b579b4189269140aeded6ae9ef6ba
zfl4wless <noah.bauditz@live.de>
2023-02-22 21:55:32 +0100

    feat: adds fade transition to scroll-down icon

--- src/lib/components/animations/ScrollDownIcon.svelte
+++ src/lib/components/animations/ScrollDownIcon.svelte
@@ -1,6 +1,22 @@
-<div
-    class="scroll-down-icon absolute bottom-4 left-1/2 -translate-x-1/2 w-5 h-10 outline outline-neutral-500 rounded-xl before:content-[''] before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:w-1 before:h-3 before:bg-neutral-500 before:rounded-full"
-/>
+<script>
+    import { fade } from 'svelte/transition';
+
+    let scrollDownIcon = true;
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+            scrollDownIcon = false;
+        } else {
+            scrollDownIcon = true;
+        }
+    });
+</script>
+
+{#if scrollDownIcon}
+    <div
+        class="scroll-down-icon absolute bottom-4 left-1/2 -translate-x-1/2 w-5 h-10 outline outline-neutral-500 rounded-xl before:content-[''] before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:w-1 before:h-3 before:bg-neutral-500 before:rounded-full"
+        transition:fade={{ duration: 200 }}
+    />
+{/if}
 
 <style>
     .scroll-down-icon::before {
