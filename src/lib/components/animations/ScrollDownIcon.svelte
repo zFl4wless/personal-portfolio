ce342eb6d9f898419ffb53eec876b3cb3f5530f8
zfl4wless <noah.bauditz@live.de>
2023-02-22 16:24:57 +0100

    feat: changes scroll-down mouse icon shape

--- src/lib/components/animations/ScrollDownIcon.svelte
+++ src/lib/components/animations/ScrollDownIcon.svelte
@@ -1,5 +1,5 @@
 <div
-    class="scroll-down-icon absolute bottom-4 left-1/2 -translate-x-1/2 w-5 h-10 outline outline-neutral-500 rounded-full before:content-[''] before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:w-1 before:h-3 before:bg-neutral-500 before:rounded-full"
+    class="scroll-down-icon absolute bottom-4 left-1/2 -translate-x-1/2 w-5 h-10 outline outline-neutral-500 rounded-xl before:content-[''] before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:w-1 before:h-3 before:bg-neutral-500 before:rounded-full"
 />
 
 <style>
