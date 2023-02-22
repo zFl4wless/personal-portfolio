f7375d3e43163f48cdb3629c5cf60c6be75aec73
zfl4wless <noah.bauditz@live.de>
2023-02-22 12:09:18 +0100

    feat: isolates Hero Section in its own component

---
+++ src/lib/components/layout/sections/Hero.svelte
@@ -0,0 +1,79 @@
+<script>
+    import ShootingStar from '$lib/components/animations/ShootingStar.svelte';
+    import Header from '$lib/components/layout/Header.svelte';
+    import TagCloud from 'TagCloud';
+    import { onMount } from 'svelte';
+
+    onMount(() =>
+        TagCloud(
+            // @ts-ignore-next-line
+            '.tag-cloud',
+            ['TypeScript', 'Figma', 'JavaScript', 'TailwindCSS', 'ReactJS', 'PostgreSQL', 'CSS', 'HTML', 'MySQL'],
+            {
+                radius: 320,
+                maxSpeed: 'normal',
+                initSpeed: 'normal',
+                direction: 135,
+                keep: true,
+                useContainerInlineStyles: true,
+                useItemInlineStyles: true,
+            },
+        ),
+    );
+</script>
+
+<section class="hero-section relative bg-[url('/background.png')] bg-center bg-no-repeat grid min-h-screen px-6">
+    <!-- Shooting Stars -->
+    <ShootingStar />
+    <ShootingStar />
+    <ShootingStar />
+
+    <!-- Header -->
+    <Header />
+
+    <!-- Hero Section Content -->
+    <div class="flex flex-col xl:flex-row items-center justify-center gap-14 2xl:gap-24">
+        <div>
+            <h1 class="text-7xl font-bold text-center xl:text-left">
+                Hi, I'm <span class="text-primary-500">Noah</span>!
+            </h1>
+            <div
+                class="mx-auto xl:mx-0 relative my-10 w-60 h-6 before:content-[''] before:block before:absolute before:w-40 before:h-1.5 before:bg-primary-400 before:rounded-full after:content-[''] after:block after:absolute after:w-40 after:h-1.5 after:bg-primary-400 after:rounded-full after:bottom-0 after:right-0"
+            />
+            <p
+                class="text-2xl text-center xl:text-left before:animate-pulse before:content-['['] before:mr-2 before:text-3xl before:text-primary-500 after:animate-pulse after:content-[']'] after:ml-2 after:text-3xl after:text-primary-500"
+            >
+                student / developer
+            </p>
+        </div>
+        <div class="hidden xl:block tag-cloud" />
+    </div>
+
+    <!-- Scroll Down Icon -->
+    <div
+        class="scroll-down-icon absolute bottom-4 left-1/2 -translate-x-1/2 w-6 h-12 outline outline-neutral-500 rounded-full before:content-[''] before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:w-1 before:h-3 before:bg-neutral-500 before:rounded-full"
+    />
+</section>
+
+<style>
+    .hero-section {
+        grid-template-rows: min-content auto;
+    }
+    .scroll-down-icon::before {
+        animation: scroll-down 2s infinite;
+    }
+    @keyframes scroll-down {
+        20% {
+            transform: translate(-50%, 0);
+            opacity: 1;
+        }
+        70% {
+            transform: translate(-50%, 6px);
+            opacity: 0;
+        }
+        100% {
+            opacity: 0;
+            transform: translate(-50%, 0);
+        }
+    }
+</style>
