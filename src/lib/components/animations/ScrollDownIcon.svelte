d6206595c39a4f0f7db9b20b821d590f725eb5af
zfl4wless <noah.bauditz@live.de>
2023-03-16 21:33:18 +0100

    feat: adds lucide icons & replaces scroll-down

--- src/lib/components/animations/ScrollDownIcon.svelte
+++ src/lib/components/animations/ScrollDownIcon.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import { fade } from 'svelte/transition';
+    import { ChevronsDown } from 'lucide-svelte';
 
     let scrollDownIcon = true;
     onMount(() => {
@@ -15,31 +16,26 @@
 </script>
 
 {#if scrollDownIcon}
-    <div
-        class="scroll-down-icon absolute bottom-4 left-1/2 -translate-x-1/2 w-5 h-10 outline outline-neutral-500 rounded-xl before:content-[''] before:absolute before:top-2 before:left-1/2 before:-translate-x-1/2 before:w-1 before:h-3 before:bg-neutral-500 before:rounded-full"
-        transition:fade={{ duration: 200 }}
-    />
+    <div class="scroll-down-icon absolute bottom-4 left-1/2 -translate-x-1/2" transition:fade={{ duration: 200 }}>
+        <ChevronsDown size={32} class="text-neutral-200" />
+    </div>
 {/if}
 
 <style>
-    .scroll-down-icon::before {
+    .scroll-down-icon {
         animation: scroll-down 2s infinite;
     }
     @keyframes scroll-down {
         20% {
             transform: translate(-50%, 0);
-            opacity: 1;
         }
         70% {
             transform: translate(-50%, 6px);
-            opacity: 0;
         }
         90% {
-            opacity: 0;
             transform: translate(-50%, 0);
         }
         100% {
-            opacity: 1;
             transform: translate(-50%, 0);
         }
     }
