e4580ec5da367f6a063e1b66e11682fc430aff9e
zfl4wless <noah.bauditz@live.de>
2023-06-22 13:51:15 +0200

    feat: adds first version of about-me section

--- src/lib/components/sections/About.svelte
+++ src/lib/components/sections/About.svelte
@@ -1,18 +1,26 @@
-<section id="projects-section" class="min-h-screen px-10 xl:px-10 2xl:px-60 py-10">
+<section id='projects-section' class='min-h-screen px-10 xl:px-10 2xl:px-60 py-10'>
     <h2
-        class="mb-16 text-4xl font-bold text-center tracking-wide before:animate-pulse {"before:content-['[']"} before:mr-2 before:text-3xl before:text-primary-500 after:animate-pulse {"after:content-[']']"} after:ml-2 after:text-3xl after:text-primary-500"
+        class="mb-16 text-4xl font-bold text-center tracking-wide before:animate-pulse before:content-['['] before:mr-2 before:text-3xl before:text-primary-500 after:animate-pulse after:content-[']'] after:ml-2 after:text-3xl after:text-primary-500"
     >
         That's me
     </h2>
 
-    <div class="grid grid-cols-2 justify-center items-center gap-20">
-        <p class="text-justify">
-            fj sd ffshdh fhusd jfj hjshd jhfhj sdhj fhjshdj fhkj jdjfhjdhjfjdh fjh jhjh djfhhjfj hjshd jhfhj sdhj
-            fhjshdj fhkj jdjfhjdhjfjdh fjh jhjh djfhhjfj hjshd jhfhj sdhj fhjshdj fhkj jdjfhjdhjfjdh fjh jhjh djfhhjfj
-            hjshd jhfhj sdhj fhjshdj fhkj jdjfhjdhjfjdh fjh jhjh djfhhjfj hjshd jhfhj sdhj fhjshdj fhkj jdjfhjdhjfjdh
-            fjh jhjh djfhhjfj hjshd jhfhj sdhj fhjshdj fhkj jdjfhjdhjfjdh fjh jhjh djfhhjfj hjshd jhfhj sdhj fhjshdj
-            fhkj jdjfhjdhjfjdh fjh jhjh djfhhjfj hjshd jhfhj sdhj fhjshdj fhkj jdjfhjdhjfjdh fjh jhjh djfhh
+    <div class='grid grid-cols-[auto_auto] justify-center items-center gap-20'>
+        <p>
+            My name is Noah and I am a 16 year old high school student from <a
+            href='https://en.wikipedia.org/wiki/Oberhausen'>Oberhausen</a> (Germany). I started programming in April
+            2020, and ever since then, I have truly fallen in love with development.
+            <br><br>
+            I did my first steps with coding by writing discord bots in python, until web development grabbed my
+            attention. Like anyone else, I started learning HTML and plain (S)CSS but then transitioned quickly to
+            frameworks like <a href='https://getbootstrap.com/'>Bootstrap</a> or <a href='https://tailwindcss.com/'>TailwindCSS</a>.
+            Making my way through JavaScript, TypeScript captured my interest and I started learning fully featured
+            frameworks like <a href='https://react.dev/'>React</a> and <a href='https://svelte.dev/'>Svelte</a>.
+            <br><br>
+            My current project is called <a href='https://hondicard.com/'>Hondicard</a>, it's a flashcard-based learning
+            application that I created together with a friend in our development team <a href='https://metahond.dev'>Metahond</a>.
         </p>
-        <img class="h-4/6 aspect-square outline outline-primary-500 rounded-lg shadow-lg" src="img/Avatar.png" alt="Noah's Avatar" />
+        <img class='h-96 aspect-square drop-shadow-image rounded-md' src='img/fl4wless.svg'
+             alt="Noah's Avatar" />
     </div>
 </section>
