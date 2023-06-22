c209f5842968bff252ca292b123d8801c24d80e3
zfl4wless <noah.bauditz@live.de>
2023-06-22 13:28:15 +0200

    improves: semantic HTML

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -33,7 +33,7 @@
 </script>
 
 <section
-    id="hero-section"
+    id='hero-section'
     class="hero-section relative bg-neutral-900 bg-[url('/img/Background.png')] bg-center bg-no-repeat bg-cover grid min-h-screen px-6 overflow-hidden"
 >
     <!-- Stars -->
@@ -43,21 +43,21 @@
     <Header />
 
     <!-- Hero Section Content -->
-    <div class="flex flex-col xl:flex-row items-center justify-center gap-14 2xl:gap-24">
+    <div class='flex flex-col xl:flex-row items-center justify-center gap-14 2xl:gap-24'>
         <div>
-            <h1 class="text-7xl font-bold text-center xl:text-left tracking-wide">
-                Hi, I'm <span class="text-primary-500">Noah</span>!
+            <h1 class='text-7xl font-bold text-center xl:text-left tracking-wide'>
+                Hi, I'm <span class='text-primary-500'>Noah</span>!
             </h1>
-            <div
-                class="mx-auto xl:mx-0 relative my-10 w-60 h-6 before:content-[''] before:block before:absolute before:w-40 before:h-1.5 before:bg-primary-400 before:rounded-full after:content-[''] after:block after:absolute after:w-40 after:h-1.5 after:bg-primary-400 after:rounded-full after:bottom-0 after:right-0"
+            <hr
+                class="border-none mx-auto xl:mx-0 relative my-10 w-60 h-6 before:content-[''] before:block before:absolute before:w-40 before:h-1.5 before:bg-primary-400 before:rounded-full after:content-[''] after:block after:absolute after:w-40 after:h-1.5 after:bg-primary-400 after:rounded-full after:bottom-0 after:right-0"
             />
             <p
-                class="text-2xl text-center xl:text-left before:animate-pulse {"before:content-['{']"} before:mr-2 before:text-3xl before:text-primary-500 after:animate-pulse {"after:content-['}']"} after:ml-2 after:text-3xl after:text-primary-500"
+                class="text-2xl text-center xl:text-left before:animate-pulse before:content-['['] before:mr-2 before:text-3xl before:text-primary-500 after:animate-pulse after:content-[']'] after:ml-2 after:text-3xl after:text-primary-500"
             >
                 student / developer
             </p>
         </div>
-        <div class="hidden xl:block tag-cloud" />
+        <div class='hidden xl:block tag-cloud'></div>
     </div>
 
     <!-- Scroll Down Icon -->
