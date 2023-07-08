5af3c6aeea23d5401468349491270a36b62dcca1
fl4wless <noah.bauditz@live.de>
2023-07-09 00:45:53 +0200

    feat: adds legal notice as dialog

---
+++ src/lib/components/LegalDialog.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+    export let dialog;
+</script>
+
+<dialog class='bg-neutral-700 text-neutral-100 rounded-xl' bind:this={dialog} on:close>
+    <div class='p-5'>
+        <h1 class='font-koulen text-3xl'>Legal Notice</h1>
+
+        <h4 class='text-xl font-medium mt-3 mb-1'>INFORMATION PURSUANT TO SECT. 5 GERMAN TELEMEDIA ACT (TMG)</h4>
+
+        <p>
+            Noah Bauditz
+            <br>
+            Untere Walsumermarkstr. 23
+            <br>
+            46147 Oberhausen
+        </p>
+
+        <h4 class='text-xl font-medium mt-3 mb-1'>Contact</h4>
+
+        <p>E-Mail: development@fl4wless.de</p>
+
+        <p class='mt-4'>Source: <a href='https://www.e-recht24.de/impressum-generator.html'>e-recht24</a></p>
+    </div>
+</dialog>
--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,17 +1,27 @@
 <script lang='ts'>
-    const messages = [
+    import LegalDialog from '../LegalDialog.svelte';
+
+    const coolMessage = [
         'Developed with ☕ and a sprinkle of magic.',
         'Made with love, caffeine, and a little bit of code.',
         'Proudly crafted by a sleep-deprived developer.',
         'Best viewed on a Friday with a cup of hot chocolate.',
         'No developers were harmed in the making of this website.',
         'Powered by sheer determination and copious amounts of pizza.',
-    ];
+    ][Math.floor(Math.random() * 6)];
+
+    let dialog;
 </script>
 
 <footer class='flex justify-between px-6 py-4 text-base'>
     <span class='text-neutral-300'>
-        {messages[Math.floor(Math.random() * messages.length)]}
+        {coolMessage}
     </span>
-    <a class='transition ease-in-out text-neutral-300 hover:brightness-125' href='/legal'>Legal Notice</a>
+
+    <button class='transition ease-in-out text-neutral-300 hover:brightness-125' on:click={() => dialog.showModal()}>
+        Legal Notice
+    </button>
+
+
+    <LegalDialog bind:dialog />
 </footer>
