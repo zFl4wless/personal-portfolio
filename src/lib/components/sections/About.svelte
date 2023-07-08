3ea055b0e5714c00cb99ae396e42e5ad21594a57
fl4wless <noah.bauditz@live.de>
2023-07-09 00:53:26 +0200

    fix: broken spacings on desktop

--- src/lib/components/sections/About.svelte
+++ src/lib/components/sections/About.svelte
@@ -2,7 +2,7 @@
     import { User, MapPin, Cpu, Globe, Code2, Bike, Joystick, Music } from 'lucide-svelte';
 </script>
 
-<section id='projects-section' class='min-h-screen flex flex-col justify-center px-6 xl:px-20 py-10'>
+<section id='projects-section' class='min-h-screen flex flex-col justify-center px-6 xl:px-20 2xl:px-60 py-10'>
     <h2
         class="mb-16 text-4xl font-koulen font-bold text-center tracking-wide before:animate-pulse before:content-['[']  before:mr-3 before:text-4xl before:text-primary-500 after:animate-pulse after:content-[']'] after:ml-3 after:text-4xl after:text-primary-500"
     >
