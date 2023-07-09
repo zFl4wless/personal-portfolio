13b634d69bb7bb38ea5a3ba901676319ac63f4ac
fl4wless <noah.bauditz@live.de>
2023-07-09 15:29:44 +0200

    feat: adds cool hover effect (maik is smart)

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -48,7 +48,12 @@
     <div class='flex flex-col xl:flex-row items-center justify-center gap-14 2xl:gap-24'>
         <div>
             <h1 class='text-7xl font-bold text-center xl:text-left tracking-wide'>
-                Hi, I'm <span class='text-primary-500'>Noah</span>!
+                Hi, I'm
+                <mark
+                    class='relative z-[1] before:content-[" "] before:z-[-1] before:block before:absolute before:inset-0 before:top-1/2 before:bg-primary-500 before:bg-opacity-20 before:scale-x-0 before:origin-bottom-right before:transition-transform before:ease-in-out before:hover:scale-x-100 before:hover:origin-bottom-left'>
+                    Noah
+                </mark>
+                !
             </h1>
             <hr
                 class="border-none mx-auto xl:mx-0 relative my-10 w-60 h-6 before:content-[''] before:block before:absolute before:w-40 before:h-1.5 before:bg-primary-400 before:rounded-full after:content-[''] after:block after:absolute after:w-40 after:h-1.5 after:bg-primary-400 after:rounded-full after:bottom-0 after:right-0"
