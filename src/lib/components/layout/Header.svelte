7ed0f98996fe93e2548249e3fe395ab3f2d7f1d0
fl4wless <noah.bauditz@live.de>
2023-07-13 16:55:27 +0200

    style: adjusts social icon's size

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,19 +1,19 @@
 <header class='flex justify-between items-center py-4'>
     <span class='font-koulen text-2xl font-thin text-neutral-100 cursor-default'>Fl4wless</span>
-    <div class='flex items-center gap-6'>
+    <div class='flex items-center gap-4 md:gap-6'>
         <a class='transition ease-in-out hover:opacity-75 cursor-pointer' href='https://github.com/zFl4wless'
            target='_blank'>
-            <img src='icons/github.svg' alt='GitHub Logo' height={32} width={32} />
+            <img src='icons/github.svg' alt='GitHub Logo' height={28} width={28} />
         </a>
 
         <a class='transition ease-in-out hover:opacity-75 cursor-pointer' href='https://instagram.com/noahbauditz'
            target='_blank'>
-            <img src='icons/instagram.svg' alt='Instagram Logo' height={32} width={32} />
+            <img src='icons/instagram.svg' alt='Instagram Logo' height={28} width={28} />
         </a>
 
         <a class='transition ease-in-out hover:opacity-75 cursor-pointer' href='https://www.buymeacoffee.com/noahbdtz'
            target='_blank'>
-            <img src='icons/buymeacoffee.svg' alt='Buymeacoffee Logo' height={32} width={32} />
+            <img src='icons/buymeacoffee.svg' alt='Buymeacoffee Logo' height={28} width={28} />
         </a>
     </div>
 </header>
