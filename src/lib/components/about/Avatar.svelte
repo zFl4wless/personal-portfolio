086f9ed82ce1d6ce09c7245b3906376f13754016
zfl4wless <noah.bauditz@live.de>
2023-07-25 16:31:50 +0200

    fix: avatar not showing on chrome

--- src/lib/components/about/Avatar.svelte
+++ src/lib/components/about/Avatar.svelte
@@ -1,4 +1,4 @@
-<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'
+<svg xmlns='http://www.w3.org/2000/svg'
      viewBox='0 10 211.73 180' width='400'>
     <defs>
         <clipPath id='background-clip'>
@@ -11,7 +11,7 @@
           d='M39 153.73s31.57 19.71 77.26 15.21 90.18-37.23 90.36-72.33-10.51-57-35.28-63-50.22 17-76.31 20-60.12-15.88-78.32 2.51S-4.88 125.2 39 153.73z'
           fill='#7E78DC' />
     <g clip-path='url(#background-clip)'>
-        <image x='12.5%' y='7%' width='75%' height='' xlink:href='img/fl4wless.svg'
+        <image x='12.5%' y='7%' width='75%' height='100%' href='img/fl4wless.svg'
                style='filter: drop-shadow(0 0 10px #485059);' />
     </g>
 </svg>
