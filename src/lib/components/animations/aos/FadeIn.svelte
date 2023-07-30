fc542ba5cfcf9ac91947435dab98a1c307753722
fl4wless <noah.bauditz@live.de>
2023-07-30 15:30:48 +0200

    feat: adds animate on scroll to ProjectItem

---
+++ src/lib/components/animations/aos/FadeIn.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+    import { fade } from 'svelte/transition';
+    import { inview } from 'svelte-inview';
+
+    let isInView;
+</script>
+
+<div
+    use:inview={{unobserveOnEnter: true}}
+    on:change={({detail}) => {
+        isInView = detail.inView;
+    }}
+>
+    {#if isInView}
+        <div in:fade>
+            <slot />
+        </div>
+    {/if}
+</div>
