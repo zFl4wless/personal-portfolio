4a8bb8a9e0cae4129a60d0da6e6c8f2a454ed4f6
zfl4wless <noah.bauditz@live.de>
2023-08-04 22:57:06 +0200

    refactor: removes aos for now

--- src/lib/components/projects/ProjectItem.svelte
+++ src/lib/components/projects/ProjectItem.svelte
@@ -1,6 +1,5 @@
 <script lang='ts'>
     import { Calendar, Radio, Code } from 'lucide-svelte';
-    import FadeIn from '../animations/aos/FadeIn.svelte';
 
     export let project: {
         title: string;
@@ -13,54 +12,52 @@
     export let index: number;
 </script>
 
-<FadeIn>
-    <div class='border-4 border-neutral-850 rounded-xl grid grid-cols-1 lg:grid-cols-[auto_auto] items-center lg:20'>
-        <div class="bg-neutral-850 p-10 {index % 2 !== 0 ? 'order-1' : 'order-2 lg:order-none'}">
-            <p class='text-2xl font-semibold mb-2'>{project.title}</p>
+<div class='border-4 border-neutral-850 rounded-xl grid grid-cols-1 lg:grid-cols-[auto_auto] items-center lg:20'>
+    <div class="bg-neutral-850 p-10 {index % 2 !== 0 ? 'order-1' : 'order-2 lg:order-none'}">
+        <p class='text-2xl font-semibold mb-2'>{project.title}</p>
 
-            <span class='inline-flex items-center gap-2 text-xs bg-neutral-700 rounded-md px-3 py-1.5 mb-6'>
+        <span class='inline-flex items-center gap-2 text-xs bg-neutral-700 rounded-md px-3 py-1.5 mb-6'>
             <Calendar size={14} />
-                {project.date}
+            {project.date}
         </span>
 
-            <p class='text-justify mb-4'>
-                <slot />
-            </p>
+        <p class='text-justify mb-4'>
+            <slot />
+        </p>
 
-            <ul class='flex flex-wrap gap-1.5'>
-                {#each project.tags as tag}
-                    <li class='flex-grow bg-neutral-700 text-xs text-center uppercase rounded-md px-8 py-1.5'>
-                        {tag}
-                    </li>
-                {/each}
-            </ul>
+        <ul class='flex flex-wrap gap-1.5'>
+            {#each project.tags as tag}
+                <li class='flex-grow bg-neutral-700 text-xs text-center uppercase rounded-md px-8 py-1.5'>
+                    {tag}
+                </li>
+            {/each}
+        </ul>
 
-            <div class='flex items-center gap-2 mt-10'>
-                {#if project.visitLink}
-                    <a
-                        href={project.visitLink}
-                        target='_blank'
-                        class='flex items-center text-base text-neutral-100 no-underline gap-1.5 bg-primary-500 px-4 py-2 rounded-md hover:brightness-110 transition ease-in-out'
-                    >
-                        <Radio size={20} />
-                        Live
-                    </a>
-                {/if}
-                {#if project.codeLink}
-                    <a
-                        href={project.codeLink}
-                        target='_blank'
-                        class='flex items-center text-base text-neutral-100 no-underline gap-1.5 bg-neutral-500 px-4 py-2 rounded-md hover:brightness-110 transition ease-in-out'
-                    >
-                        <Code size={20} />
-                        Code
-                    </a>
-                {/if}
-            </div>
-        </div>
-
-        <div class='grid place-items-center w-[30rem] p-5 mx-auto'>
-            <img src={project.image} alt={project.title} />
+        <div class='flex items-center gap-2 mt-10'>
+            {#if project.visitLink}
+                <a
+                    href={project.visitLink}
+                    target='_blank'
+                    class='flex items-center text-base text-neutral-100 no-underline gap-1.5 bg-primary-500 px-4 py-2 rounded-md hover:brightness-110 transition ease-in-out'
+                >
+                    <Radio size={20} />
+                    Live
+                </a>
+            {/if}
+            {#if project.codeLink}
+                <a
+                    href={project.codeLink}
+                    target='_blank'
+                    class='flex items-center text-base text-neutral-100 no-underline gap-1.5 bg-neutral-500 px-4 py-2 rounded-md hover:brightness-110 transition ease-in-out'
+                >
+                    <Code size={20} />
+                    Code
+                </a>
+            {/if}
         </div>
     </div>
-</FadeIn>
+
+    <div class='grid place-items-center w-[30rem] p-5 mx-auto'>
+        <img src={project.image} alt={project.title} />
+    </div>
+</div>
