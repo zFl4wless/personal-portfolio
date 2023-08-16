9710e7d833f52151cb17cff9445c047a20f081a0
zfl4wless <noah.bauditz@live.de>
2023-08-16 11:01:08 +0200

    feat: adds 404 page

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import "../../app.css";
+</script>
+
+<main
+  class="bg-neutral-900 bg-[url('/img/Background.png')] bg-center bg-no-repeat bg-cover min-h-screen flex flex-col justify-center items-center">
+  <h1 class="font-koulen text-[12rem] tracking-widest leading-tight">404</h1>
+  <p class="text-center"><span class="text-primary-400 font-medium uppercase">LOST IN SPACE?</span><br>Hmm, looks like that page doesn't exist.</p>
+  <a
+    href="/"
+    class="flex items-center text-sm capitalize text-neutral-300 border border-neutral-300 no-underline gap-1.5 px-5 py-2 mt-5 rounded-md hover:text-primary-400 hover:border-primary-400 transition ease-in-out"
+  >
+    Take me home!
+  </a>
+</main>
