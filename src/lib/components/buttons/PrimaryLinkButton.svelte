21fe6b8ff50a668b08aa4608692745a5f3427f90
zfl4wless <noah.bauditz@live.de>
2023-09-11 10:03:56 +0200

    style: improves button design

---
+++ src/lib/components/buttons/PrimaryLinkButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a href={href}
+   class="no-underline overflow-hidden rounded relative inline-flex group items-center justify-center px-3.5 py-2 m-1 cursor-pointer border-b-4 border-l-2 active:border-primary-600 active:shadow-none shadow-lg bg-gradient-to-tr from-primary-600 to-primary-500 border-primary-700 text-white">
+  <span
+    class="absolute w-0 h-0 transition-all duration-300 ease-out bg-neutral-100 rounded-full group-hover:w-32 group-hover:h-32 opacity-10"></span>
+  <span class="relative flex items-center gap-1.5">
+    <slot />
+  </span>
+</a>
--- src/lib/components/projects/ProjectItem.svelte
+++ src/lib/components/projects/ProjectItem.svelte
@@ -1,65 +1,59 @@
 <script lang="ts">
-	import Calendar from '$lucide/calendar.svelte';
-	import Radio from '$lucide/radio.svelte';
-	import Code from '$lucide/code.svelte';
+  import Calendar from "$lucide/calendar.svelte";
+  import Radio from "$lucide/radio.svelte";
+  import Code from "$lucide/code.svelte";
+  import PrimaryLinkButton from "$lib/components/buttons/PrimaryLinkButton.svelte";
+  import NeutralLinkButton from "$lib/components/buttons/NeutralLinkButton.svelte";
 
-	export let project: {
-		title: string;
-		date: string;
-		tags: string[];
-		visitLink?: string;
-		codeLink?: string;
-		image: string;
-	};
-	export let index: number;
+  export let project: {
+    title: string;
+    date: string;
+    tags: string[];
+    visitLink?: string;
+    codeLink?: string;
+    image: string;
+  };
+  export let index: number;
 </script>
 
 <div class="border-4 border-neutral-850 rounded-xl grid grid-cols-1 lg:grid-cols-[auto_auto] items-center lg:20">
-	<div class="bg-neutral-850 p-10 {index % 2 !== 0 ? 'order-1' : 'order-2 lg:order-none'}">
-		<p class="text-2xl font-semibold mb-2">{project.title}</p>
+  <div class="bg-neutral-850 p-10 {index % 2 !== 0 ? 'order-1' : 'order-2 lg:order-none'}">
+    <p class="text-2xl font-semibold mb-2">{project.title}</p>
 
-		<span class="inline-flex items-center gap-2 text-xs bg-neutral-700 rounded-md px-3 py-1.5 mb-6">
+    <span class="inline-flex items-center gap-2 text-xs bg-neutral-700 rounded-md px-3 py-1.5 mb-6">
 			<Calendar size={14} />
-			{project.date}
+      {project.date}
 		</span>
 
-		<p class="text-justify mb-4">
-			<slot />
-		</p>
+    <p class="text-justify mb-4">
+      <slot />
+    </p>
 
-		<ul class="flex flex-wrap gap-1.5">
-			{#each project.tags as tag}
-				<li class="flex-grow bg-neutral-700 text-xs text-center uppercase rounded-md px-8 py-1.5">
-					{tag}
-				</li>
-			{/each}
-		</ul>
+    <ul class="flex flex-wrap gap-1.5">
+      {#each project.tags as tag}
+        <li class="flex-grow bg-neutral-700 text-xs text-center uppercase rounded-md px-8 py-1.5">
+          {tag}
+        </li>
+      {/each}
+    </ul>
 
-		<div class="flex items-center gap-2 mt-10">
-			{#if project.visitLink}
-				<a
-					href={project.visitLink}
-					target="_blank"
-					class="flex items-center text-base text-neutral-100 no-underline gap-1.5 bg-primary-500 px-4 py-2 rounded-md hover:brightness-110 transition ease-in-out"
-				>
-					<Radio size={20} />
-					Live
-				</a>
-			{/if}
-			{#if project.codeLink}
-				<a
-					href={project.codeLink}
-					target="_blank"
-					class="flex items-center text-base text-neutral-100 no-underline gap-1.5 bg-neutral-500 px-4 py-2 rounded-md hover:brightness-110 transition ease-in-out"
-				>
-					<Code size={20} />
-					Code
-				</a>
-			{/if}
-		</div>
-	</div>
+    <div class="flex items-center gap-2 mt-10">
+      {#if project.visitLink}
+        <NeutralLinkButton href={project.visitLink}>
+          <Radio size={20} />
+          Live
+        </NeutralLinkButton>
+      {/if}
+      {#if project.codeLink}
+        <PrimaryLinkButton href={project.codeLink}>
+          <Code size={20} />
+          Code
+        </PrimaryLinkButton>
+      {/if}
+    </div>
+  </div>
 
-	<div class="grid place-items-center w-[30rem] p-5 mx-auto">
-		<img src={project.image} alt={project.title} />
-	</div>
+  <div class="grid place-items-center w-[30rem] p-5 mx-auto">
+    <img src={project.image} alt={project.title} />
+  </div>
 </div>
